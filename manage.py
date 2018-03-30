321a1a76bd7135a9cce2fc384a901197d9812925
xwp <xwp_fullstack@163.com>
2018-03-30 20:28:52 +0800

    配置redis数据库信息

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@
 #导入Flask
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import redis
 
 
 
@@ -12,10 +13,16 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/iHome'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # 配置redis
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
 
 app = Flask(__name__)
 
+
+
 db = SQLAlchemy(app)
+
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 #注册数据库
 app.config.from_object(Config)
 #定义视图函数
