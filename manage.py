635e65aa3d864a86acc5206aee39a15bd991125f
xwp <xwp_fullstack@163.com>
2018-03-30 20:19:27 +0800

    配置mysql数据库信息

--- manage.py
+++ manage.py
@@ -2,22 +2,26 @@
 
 #导入Flask
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 class Config(object):
     DEBUG = True
-
     #配置数据库
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/iHome'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     # 配置redis
 
 app = Flask(__name__)
 
+db = SQLAlchemy(app)
 #注册数据库
 app.config.from_object(Config)
 #定义视图函数
 @app.route('/')
 def index():
-    return
+    return 'index'
 
 #启动该应用的入口
 if __name__ == '__main__':
